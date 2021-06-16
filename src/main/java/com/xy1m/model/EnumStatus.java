73bbca6f2d0419c751c9223d762f0d10a9dc1ebd
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2021-06-16 16:11:54 -0500

    BAC-5072 - added missing status.

--- src/main/java/com/xy1m/model/EnumStatus.java
+++ src/main/java/com/xy1m/model/EnumStatus.java
@@ -1,5 +1,6 @@
 package com.xy1m.model;
 
 public enum EnumStatus {
-    active, content_in_review, balance_issue, budget_exhausted, inactive, disabled, archived, non_archived, pending
+    active, content_in_review, balance_issue, budget_exhausted, inactive, disabled, archived, non_archived, pending,
+    content_denied
 }
